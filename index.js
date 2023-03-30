87e305a2838857744af689dbc25d4e6348e5cd61
rick <55769589+richardrso@users.noreply.github.com>
2023-03-30 11:53:40 -0300

    Add configuration for Discord.js and OpenAI API keys using environment variables

---
+++ index.js
@@ -0,0 +1,7 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+require("dotenv/config");
+const { OpenAIApi, Configuration } = require("openai");
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
