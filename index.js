e5ce05794f79e99c97db3a0fe7ee02e3d994bef0
rick <55769589+richardrso@users.noreply.github.com>
2023-03-30 12:02:20 -0300

    Refactor code to improve readability and add functionality for handling incoming messages

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ const client = new Client({
 client.once(Events.ClientReady, (clientUser) => {
     console.log(`Logged in as ${clientUser.user.tag}`);
   });
+
+  client.login(process.env.BOT_TOKEN);
+
+const BOT_CHANNEL = "1084538798744010762";
+const PAST_MESSAGES = 5;
+const COOLDOWN_PERIOD = 0; // 30 seconds
+const cooldowns = new Map();
+
+client.on(Events.MessageCreate, async (message) => {
+  try {
+    if (message.author.bot) return;
+    if (message.channel.id !== BOT_CHANNEL) return;
+
+    console.log(`Received message: "${message.content}" from ${message.member.displayName}`);
