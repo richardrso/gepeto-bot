6f9597cb5212e28bcf59d884140a144fc6e4a252
rick <55769589+richardrso@users.noreply.github.com>
2023-03-30 11:56:21 -0300

    Initialize OpenAI API and Discord client with specified intents

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const { OpenAIApi, Configuration } = require("openai");
 const config = new Configuration({
   apiKey: process.env.OPENAI_KEY,
 });
+
+const openai = new OpenAIApi(config);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
